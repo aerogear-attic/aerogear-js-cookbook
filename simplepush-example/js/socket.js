3576a5a3dee7b68a9d8e002ec33fdedfddf80616
Kris Borchers <kris.borchers@gmail.com>
2013-07-29 11:10:32 -0500

    Use latest changes to SimplePush API

--- simplepush-example/js/socket.js
+++ simplepush-example/js/socket.js
@@ -1,55 +1,57 @@
 (function() {
-    var mailEndpoint, mailRequest, fooEndpoint, fooRequest, broadcastRequest, broadcastEndpoint, testFrame,
+    var mailEndpoint, mailRequest, fooEndpoint, fooRequest, broadcastRequest, broadcastEndpoint, testFrame, SPClient,
         // Obviously this isn't secure but what we have for now
-        UPClient = AeroGear.UnifiedPushClient( AeroGear.SimplePush.variantID, "0dcdc9ec-495c-4c2d-a675-b61facbb7247", "http://localhost:8080/ag-push/rest/registry/device" );
+        UPClient = AeroGear.UnifiedPushClient( "6c73c51c-f4d4-45da-800a-5743501bf4e0", "5cdc4870-42b7-44bd-854a-c80279a6e2dc", "http://localhost:8080/ag-push/rest/registry/device" );
 
-    getTextAreaElement().value = "Web Socket opened!";
+    function spConnect() {
+        getTextAreaElement().value = "Web Socket opened!";
 
-    broadcastRequest = navigator.push.register();
-    broadcastRequest.onsuccess = function( event ) {
-        broadcastEndpoint = event.target.result;
-        UPClient.registerWithPushServer( "broadcast", broadcastEndpoint );
-        appendTextArea("Subscribed to Broadcast messages on " + broadcastEndpoint.channelID);
-        $("#broadcastVersion").val( localStorage.getItem( broadcastEndpoint.channelID ) || 1 );
-        $("#broadcast").prop("disabled", false);
-    };
+        broadcastRequest = navigator.push.register();
+        broadcastRequest.onsuccess = function( event ) {
+            broadcastEndpoint = event.target.result;
+            UPClient.registerWithPushServer( "broadcast", broadcastEndpoint );
+            appendTextArea("Subscribed to Broadcast messages on " + broadcastEndpoint.channelID);
+            $("#broadcastVersion").val( localStorage.getItem( broadcastEndpoint.channelID ) || 1 );
+            $("#broadcast").prop("disabled", false);
+        };
 
-    mailRequest = navigator.push.register();
-    mailRequest.onsuccess = function( event ) {
-        mailEndpoint = event.target.result;
-        UPClient.registerWithPushServer( "mail", mailEndpoint, "test@test.com" );
-        $("#mailVersion").attr("name", mailEndpoint.channelID);
-        appendTextArea("Subscribed to Mail messages on " + mailEndpoint.channelID);
-        $("#mailVersion").val( localStorage.getItem( mailEndpoint.channelID ) || 1 );
-        $("#mail").prop("disabled", false);
-    };
+        mailRequest = navigator.push.register();
+        mailRequest.onsuccess = function( event ) {
+            mailEndpoint = event.target.result;
+            UPClient.registerWithPushServer( "mail", mailEndpoint, "test@test.com" );
+            $("#mailVersion").attr("name", mailEndpoint.channelID);
+            appendTextArea("Subscribed to Mail messages on " + mailEndpoint.channelID);
+            $("#mailVersion").val( localStorage.getItem( mailEndpoint.channelID ) || 1 );
+            $("#mail").prop("disabled", false);
+        };
 
-    fooRequest = navigator.push.register();
-    fooRequest.onsuccess = function( event ) {
-        fooEndpoint = event.target.result;
-        UPClient.registerWithPushServer( "foo", fooEndpoint, "test@test.com" );
-        $("#fooVersion").attr("name", fooEndpoint.channelID);
-        appendTextArea("Subscribed to Foo messages on " + fooEndpoint.channelID);
-        $("#fooVersion").val( localStorage.getItem( fooEndpoint.channelID ) || 1 );
-        $("#foo").prop("disabled", false);
-    };
+        fooRequest = navigator.push.register();
+        fooRequest.onsuccess = function( event ) {
+            fooEndpoint = event.target.result;
+            UPClient.registerWithPushServer( "foo", fooEndpoint, "test@test.com" );
+            $("#fooVersion").attr("name", fooEndpoint.channelID);
+            appendTextArea("Subscribed to Foo messages on " + fooEndpoint.channelID);
+            $("#fooVersion").val( localStorage.getItem( fooEndpoint.channelID ) || 1 );
+            $("#foo").prop("disabled", false);
+        };
 
-    navigator.setMessageHandler( "push", function( message ) {
-        if ( message.channelID === mailEndpoint.channelID ) {
-            appendTextArea("Mail Notification - " + message.version);
-            $("#mailVersion").val( +message.version + 1 );
-            localStorage.setItem( message.channelID, +message.version + 1 );
-        } else if ( message.channelID === fooEndpoint.channelID ) {
-            appendTextArea("Foo Notification - " + message.version);
-            $("#fooVersion").val( +message.version  + 1);
-            localStorage.setItem( message.channelID, +message.version + 1 );
-        } else if ( message.channelID === broadcastEndpoint.channelID ) {
-            // Broadcast messages are subscribed by default and can be acted on as well
-            appendTextArea("Broadcast Notification - " + message.version);
-            $("#broadcastVersion").val( +message.version + 1 );
-            localStorage.setItem( message.channelID, +message.version + 1 );
-        }
-    });
+        navigator.setMessageHandler( "push", function( message ) {
+            if ( message.channelID === mailEndpoint.channelID ) {
+                appendTextArea("Mail Notification - " + message.version);
+                $("#mailVersion").val( +message.version + 1 );
+                localStorage.setItem( message.channelID, +message.version + 1 );
+            } else if ( message.channelID === fooEndpoint.channelID ) {
+                appendTextArea("Foo Notification - " + message.version);
+                $("#fooVersion").val( +message.version  + 1);
+                localStorage.setItem( message.channelID, +message.version + 1 );
+            } else if ( message.channelID === broadcastEndpoint.channelID ) {
+                // Broadcast messages are subscribed by default and can be acted on as well
+                appendTextArea("Broadcast Notification - " + message.version);
+                $("#broadcastVersion").val( +message.version + 1 );
+                localStorage.setItem( message.channelID, +message.version + 1 );
+            }
+        });
+    }
 
     function appendTextArea(newData) {
         var el = getTextAreaElement();
@@ -94,7 +96,7 @@
             dataType: "json",
             type: "POST",
             headers: {
-                "Authorization": "Basic " + window.btoa("fbe97091-af1b-4cc0-aa9c-9fcae3f170c7:2532ad80-f2d9-4e2a-a9f3-528b5d794368")
+                "Authorization": "Basic " + window.btoa("491a4e13-f698-4cff-af94-c07329d1671e:3357a4c5-89d9-4e06-965f-884f75a99fda")
             },
             data: JSON.stringify( data ),
             complete: function() {
@@ -102,4 +104,6 @@
             }
         });
     });
+
+    SPClient = AeroGear.SimplePushClient( "http://localhost:7777/simplepush", spConnect )
 })();
