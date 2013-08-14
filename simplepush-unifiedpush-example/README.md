257d47fa19a79604ea7609ed630ce18fa71334b2
Christos Vasilakis <cvasilak@gmail.com>
2013-08-14 18:58:56 +0300

    initial commit.

---
+++ simplepush-unifiedpush-example/README.md
@@ -0,0 +1,86 @@
+aerogear-simplepush-unifiedpush-quickstart
+==============================
+
+An example of using AeroGear's SimplePush/Unified Push JavaScript API. Based on [aerogear-simplepush-quickstart](https://github.com/aerogear/aerogear-simplepush-quickstart) enchancing to support registration with the UnifiedPush server.
+
+Please visit our [tutorial page](http://www.aerogear.org/docs/guides/aerogear-push-js/) for more information.
+
+Getting started!
+----------------
+
+#### Prerequisites
+
+* Java 6.0 (Java SDK 1.6)+
+* Maven
+* JBoss Application Server 7.1.1 or later (e.g. WildFly)
+* AeroGear UnifiedPush server
+* AeroGear SimplePush server
+
+### Building the AeroGear SimplePush Server
+
+We need to build the [AeroGear SimplePush Server](https://github.com/aerogear/aerogear-simplepush-server). A few prerequisites are required before you can build the actual SimplePush Server:
+
+The Netty SockJS branch:
+
+    git clone https://github.com/danbev/netty/tree/ci
+    cd ci
+    mvn install -DskipTests=true
+
+Download the server from the [AeroGear SimplePush Server](https://github.com/aerogear/aerogear-simplepush-server) project’s github page and build the source code:
+
+    git clone git@github.com:aerogear/aerogear-simplepush-server.git
+    cd aerogear-simplepush-server
+    mvn install -DskipTests=true
+
+Now perform a ```cd server-netty``` and execute the following command to start the server on your machine:
+
+
+    mvn exec:java -Dexec.args="-host=localhost -port=7777 -tls=false -ack_interval=10000 -useragent_reaper_timeout=60000 -token_key=yourRandomToken" 
+
+This starts an _unsecured_ instance of the AeroGear SimplePush Server on localhost using port 7777.
+
+**_NOTE_:** The server uses an in-memory database for this demo. None of the SimplePush channels are persisted by the server. A restart of the server means that the previous registered channels are gone.
+
+### Building the AeroGear UnifiedPush Server
+
+Download the server from the [AeroGear UnifiedPush Server](https://github.com/aerogear/aerogear-unifiedpush-server) project’s github page. First, start your JBoss Application server and once that is done, simple execute on the command line the following:
+
+    cd aerogear-unified-push-server
+    mvn package jboss-as:deploy
+
+This will deploy the UnifiedPush server on your running JBoss installation, ready to accept connections. Now it’s time to perform the initial registrations with the server
+
+
+### Registration of the SimplePush Variant
+
+Once the UnifiedPush server is running, access the administration console via ```http://SERVER:PORT/ag-push``` to register your SimplePush variant.
+Temporarily there is an "admin:123" user created by default, so you can use those credentials to login. On _first_ login,  you will be requested to change the password.
+
+
+### JavaScript client
+
+Prior to run the application we need to configure the variantId and variantSecret as well as the URL's of the running servers. Navigate to _js/_ folder and open in your favorite text editor, the _unifiedpush.js_ file. The file contains the main logic of the application and is responsible to register the channels with the SimplePush server, retrieve the endpoints for the update, and then register those endpoints with the UnifiedPush server so that notifications can be received.
+
+On top of the file you will notice the configuration params that you need to change according to your specific configuration:
+    
+    // config params for UnifiedPush server
+    var variantId = "<Your-Variant-ID";
+    var variantSecret = "<Your-Variant-Secret>";
+    var simplePushUrl = "<URL of the running SimplePush server>"
+    var unifiedPushUrl = "<URL of the running UnifiedPush server>";
+
+Once that is done, open the ```index.html``` file in your favourite browser. You should see the following:
+
+![SimplePush/UnifiedPush main screen](https://raw.github.com/cvasilak/aerogear.org/6be25e8f32a15d34e9ba8f33077394bc3e9e70c2/docs/guides/aerogear-push-js/img/unifiedpush_main_screen.png "SimplePush/UnifiedPush main screen")
+
+Now, issue the following CURL command to send a push notification against the UnifiedPush server. Don't forget to replace the ```PushApplicationID``` and ```MasterSecret``` you received when you registered you Application in the administration console:
+
+    curl -u "{PushApplicationID}:{MasterSecret}"
+        -v -H "Accept: application/json" -H "Content-type: application/json"
+        -X POST
+        -d '{ "key":"value", "alert":"HELLO!", "sound":"default", "badge":7,
+             "simple-push":"version=123"}' http://localhost:8080/ag-push/rest/sender/broadcast
+
+Notice the message being received by the web application!
+
+### Have fun!
---
+++ simplepush-unifiedpush-example/js/unifiedpush.js
@@ -0,0 +1,118 @@
+(function() {
+    var mailEndpoint, mailRequest, SPClient, UPClient;
+
+    // config params for UnifiedPush server
+    var variantId = "<Your-Variant-ID";
+    var variantSecret = "<Your-Variant-Secret>";
+    var simplePushUrl = "<URL of the running SimplePush server>"
+    var unifiedPushUrl = "<URL of the running UnifiedPush server>";
+
+     // create the 'UnifiedPush' client object:
+    var UPClient = AeroGear.UnifiedPushClient(variantId, variantSecret, unifiedPushUrl + "/rest/registry/device");
+
+    // onConnect callback function called when SimplePush 
+    // successfully establishes connection to the server
+    function spConnect() {
+        appendTextArea("Connection established with SimplePush server!");
+
+        // use 'PushManager' to request a new PushServer URL endpoint for 'Mail' notifications:
+        mailRequest = navigator.push.register();
+         // the DOMRequest returns 'successfully':
+        mailRequest.onsuccess = function( event ) {
+            // extract the endpoint object from the event: 
+            mailEndpoint = event.target.result;
+            
+            appendTextArea("Subscribed to 'Mail' messages on channel " + mailEndpoint.channelID);
+
+            // if it is the first registration, need to register 
+            // the 'pushEndpoint' with the UnifiedPush server.
+            if ( mailEndpoint.pushEndpoint ) {
+                // assemble the metadata for registration with the UnifiedPush server
+                var metadata = {
+                    deviceToken: mailEndpoint.channelID,
+                    simplePushEndpoint: mailEndpoint.pushEndpoint,
+                    alias: "john",
+                    category: "email"
+                };
+
+                // register with the server
+                UPClient.registerWithPushServer(metadata);
+
+                appendTextArea("Registered 'Mail' endpoint with UnifiedPush server!")
+
+            } else {
+                appendTextArea("'Mail' was already registered!");
+            }
+        };
+
+        // let's request another endpoint for 'broadcast' notifications
+
+        // use 'PushManager' to request a new PushServer URL endpoint for 'broadcast' notifications:
+        broadcastRequest = navigator.push.register();
+         // the DOMRequest returns 'successfully':
+        broadcastRequest.onsuccess = function( event ) {
+            // extract the endpoint object from the event: 
+            broadcastEndpoint = event.target.result;
+            
+            appendTextArea("Subscribed to 'broadcast' messages on channel " + broadcastEndpoint.channelID);
+
+            // if it is the first registration, need to register 
+            // the 'pushEndpoint' with the UnifiedPush server.
+            if ( broadcastEndpoint.pushEndpoint ) {
+                // assemble the metadata for registration with the UnifiedPush server
+                var metadata = {
+                    deviceToken: broadcastEndpoint.channelID,
+                    simplePushEndpoint: broadcastEndpoint.pushEndpoint,
+                    alias: "john",
+                    category: "broadcast"
+                };
+
+                // register with the server
+                UPClient.registerWithPushServer(metadata);
+
+                appendTextArea("Registered 'broadcast' endpoint with UnifiedPush server!")
+
+            } else {
+                appendTextArea("'broadcast' was already registered!");
+            }
+        };        
+
+        // set the notification handler:
+        navigator.setMessageHandler( "push", function( message ) {
+            if ( message.channelID === mailEndpoint.channelID ) {
+                // let's react on the 'email' endpoint
+                appendTextArea("Mail Notification - " + message.version);
+            } else if ( message.channelID === broadcastEndpoint.channelID ) { 
+                // let's react on the 'broadcast' endpoint
+                appendTextArea("Broadcast Notification - " + message.version);            
+            }
+        });
+    }
+
+    function appendTextArea(newData) {
+        var el = getTextAreaElement();
+        el.value = el.value + '\n' + newData;
+    }
+
+    function getTextAreaElement() {
+        return document.getElementById('responseText');
+    }
+
+    // custom.....
+    $("#reconnect").on("click", function(event) {
+        // AeroGear add-on to allow a reconnect, if the WebSocket/SockJS connection is lost
+            navigator.push.reconnect();
+        });
+        
+    // onClose callback function:
+    function spClose() {
+        $("#reconnect").show();
+        appendTextArea("\nConnection Lost!\n");
+    }
+
+    var SPClient = AeroGear.SimplePushClient({
+        simplePushServerURL: simplePushUrl,
+        onConnect: spConnect,
+        onClose: spClose
+   });
+})();
