fc61e0145c436a2177c68a67271b71ca8b699f32
Lucas Holmquist <lholmqui@redhat.com>
2015-01-22 12:33:47 -0500

    should be localhost

--- diff-sync/app/routes/index.js
+++ diff-sync/app/routes/index.js
@@ -6,7 +6,7 @@ export default Ember.Route.extend({
 
         // To test on a real device,  be sure to change the serverURL to your IP
         var syncClient = AeroGear.DiffSyncClient({
-                serverUrl: 'ws://192.168.1.7:7777/sync',
+                serverUrl: 'ws://localhost:7777/sync',
                 onopen: controller.get('_onopen').bind(controller),
                 onsync: controller.get('_onsync').bind(controller)
             });
